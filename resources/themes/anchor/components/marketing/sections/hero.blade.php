2f8606ac543541d53a217561d6ed989904f6e89b
gabrieldeleles <carby.gabriel@icloud.com>
2025-01-13 15:55:51 -0500

    remove image home

--- resources/themes/anchor/components/marketing/sections/hero.blade.php
+++ resources/themes/anchor/components/marketing/sections/hero.blade.php
@@ -13,9 +13,9 @@
                 <x-button size="lg" color="secondary" class="w-full lg:w-auto">Learn More.</x-button>
             </div>
         </div>
-        <div class="flex items-center justify-center w-full mt-12 lg:w-1/2 lg:mt-0">
+        <!-- <div class="flex items-center justify-center w-full mt-12 lg:w-1/2 lg:mt-0">
             <img alt="Wave Character" class="relative w-full lg:scale-125 xl:translate-x-6" src="/wave/img/character.png" style="max-width:450px;">
-        </div>
+        </div> -->
     </div>
     <div class="flex-shrink-0 lg:h-[150px] flex border-t border-zinc-200 items-center w-full bg-white">
         <div class="grid h-auto grid-cols-1 px-8 py-10 mx-auto space-y-5 divide-y max-w-7xl lg:space-y-0 lg:divide-y-0 divide-zinc-200 lg:py-0 lg:divide-x md:px-12 lg:px-20 lg:divide-zinc-200 lg:grid-cols-3">
@@ -24,13 +24,13 @@
                     Personalized Call Management
                 </h3>
                 <p class="mt-2 text-sm font-medium text-zinc-500">
-                Design call flows tailored . <span class="hidden lg:inline">to your business needs</span>
+                Design call flows tailored to your business needs <span class="hidden lg:inline"></span>
                 </p>
             </div>
             <div class="pt-5 lg:pt-0 lg:px-10">
-                <h3 class="font-medium text-zinc-900">Pain Points</h3>
+                <h3 class="font-medium text-zinc-900">Call flow builder</h3>
                 <p class="mt-2 text-sm text-zinc-500">
-                    What are some pain points that your SaaS aims to solve? <span class="hidden lg:inline">Explain here.</span>
+                 powerful call flow builder that enables seamless customer interactions <span class="hidden lg:inline">Explain here.</span>
                 </p>
             </div>
             <div class="pt-5 lg:pt-0 lg:px-10">
