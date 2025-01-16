2bea6f603e3f75efbc3609658fce4f394ef696ee
gabrieldeleles <carby.gabriel@icloud.com>
2025-01-15 23:46:25 -0500

    text area

--- resources/themes/anchor/pages/projects/index.blade.php
+++ resources/themes/anchor/pages/projects/index.blade.php
@@ -131,7 +131,7 @@
         <x-app.container class="max-w-5xl">
             <div class="flex items-center justify-between mb-5">
                 <x-app.heading title="Projects" description="Check out your projects below" :border="false"/>
-                <x-modal id="create-project" width="md" :slide-over="true">
+                <!-- <x-modal id="create-project" width="md" :slide-over="true">
                     <x-slot name="trigger">
                         <x-button>New Project</x-button>
                     </x-slot>
@@ -146,7 +146,7 @@
                             </x-button>
                         </div>
                     </form>
-                </x-modal>
+                </x-modal> -->
             </div>
             <div class="overflow-x-auto border rounded-lg">
                 {{ $this->table }}
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
             ],
             refresh: [
                 `resources/themes/${activeTheme}/**/*`,
+                  `app/Livewire/**`
             ],
         }),
     ],
