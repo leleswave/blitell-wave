77330c5cfc7e41562d7a20eda649afb3c4623338
gabrieldeleles <carby.gabriel@icloud.com>
2025-01-15 23:54:14 -0500

    teste

--- resources/themes/anchor/pages/projects/index.blade.php
+++ resources/themes/anchor/pages/projects/index.blade.php
@@ -131,7 +131,8 @@
         <x-app.container class="max-w-5xl">
             <div class="flex items-center justify-between mb-5">
                 <x-app.heading title="Projects" description="Check out your projects below" :border="false"/>
-                <!-- <x-modal id="create-project" width="md" :slide-over="true">
+    
+                <x-filament-actions::modals  id="create-project" width="md" :slide-over="true">
                     <x-slot name="trigger">
                         <x-button>New Project</x-button>
                     </x-slot>
@@ -146,7 +147,7 @@
                             </x-button>
                         </div>
                     </form>
-                </x-modal> -->
+                </x-modal>
             </div>
             <div class="overflow-x-auto border rounded-lg">
                 {{ $this->table }}
